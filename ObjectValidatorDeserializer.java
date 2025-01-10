71709616c5e4879ba253587afdd7437eee4503d7
prabhat2505 <prabhat.rana828@gmail.com>
2025-01-10 14:26:39 +0530

    Update ObjectValidatorDeserializer.java

--- ObjectValidatorDeserializer.java
+++ ObjectValidatorDeserializer.java
@@ -6,28 +6,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
-public class GenericObjectValidatorDeserializer<T> extends JsonDeserializer<T> {
+public class RequestClassValidatorDeserializer<T> extends JsonDeserializer<T> {
 
     private final Class<T> targetClass;
 
-    // Constructor to set the target class dynamically
-    public GenericObjectValidatorDeserializer(Class<T> targetClass) {
+    public RequestClassValidatorDeserializer(Class<T> targetClass) {
         this.targetClass = targetClass;
     }
 
     @Override
     public T deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        // Parse the JSON node
         JsonNode node = p.readValueAsTree();
 
-        // Validate that the node is a JSON object
+        // Ensure the incoming JSON is an object
         if (!node.isObject()) {
             ctxt.reportInputMismatch(targetClass, 
-                "Expected a JSON object but found: " + node.getNodeType());
+                "Invalid JSON input: Expected a JSON object but found " + node.getNodeType());
         }
 
-        // Use ObjectMapper to deserialize the node into the target class
+        // Deserialize into the target class
         ObjectMapper mapper = (ObjectMapper) p.getCodec();
-        return mapper.treeToValue(node, targetClass);
+        T obj = mapper.treeToValue(node, targetClass);
+
+        // Perform additional validations if required
+        if (obj == null) {
+            ctxt.reportInputMismatch(targetClass, 
+                "Request body cannot be null or invalid for target type " + targetClass.getSimpleName());
+        }
+
+        return obj;
     }
 }
